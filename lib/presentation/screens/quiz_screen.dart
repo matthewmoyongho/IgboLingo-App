dc7ed5fd28a87820fcf62da40086a1f703125ff8
Matthew Moyongho <moyonghomatthew@gmail.com>
2023-08-19 06:50:07 +0100

    Quiz feature Still

--- lib/presentation/screens/quiz_screen.dart
+++ lib/presentation/screens/quiz_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:igbo_lang_tutor/core/constants.dart';
-import 'package:igbo_lang_tutor/presentation/screens/questions_screen.dart';
+
+import '../widgets/lecture_tile.dart';
 
 class QuizScreen extends StatefulWidget {
   const QuizScreen({
@@ -31,15 +32,55 @@ class _QuizState extends State<QuizScreen> {
             const SizedBox(
               height: 25,
             ),
-            GestureDetector(
-              onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => QuestionsScreen())),
-              child: Container(
-                height: 50,
-                width: double.infinity,
-                color: kPrimaryColor,
+            // GestureDetector(
+            //   onTap: () => Navigator.of(context).push(
+            //       MaterialPageRoute(builder: (context) => QuestionsScreen())),
+            //   child: Container(
+            //     height: 50,
+            //     width: double.infinity,
+            //     color: kPrimaryColor,
+            //   ),
+            // ),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 5 / 3.5,
+                children: [
+                  LectureTile(
+                    text: 'Greetings',
+                    color: Color(0XFFF4784E),
+                    secondColor: Color(0XFFEC4812),
+                    numberOfCourses: 1,
+                  ),
+                  LectureTile(
+                    text: 'Counting',
+                    color: Color(0XFF7593E9),
+                    secondColor: Color(0XFF3765EC),
+                    numberOfCourses: 1,
+                  ),
+                  LectureTile(
+                    text: 'Names',
+                    color: Color(0XFFF5E24B),
+                    secondColor: Color(0XFFF2D701),
+                    numberOfCourses: 1,
+                  ),
+                  LectureTile(
+                    text: 'Simple Sentences',
+                    color: Color(0XFF979AC6),
+                    secondColor: Color(0XFF777BB3),
+                    numberOfCourses: 1,
+                  ),
+                  LectureTile(
+                    text: 'Greetings',
+                    color: Color(0XFFA179DC),
+                    secondColor: Color(0XFFA179DC).withBlue(250),
+                    numberOfCourses: 1,
+                  ),
+                ],
               ),
-            )
+            ),
           ],
         ),
       ),
